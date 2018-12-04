19f53b93f8f4ada99312cd019a503955f0fa009f
MarcBernstein0 <marcseth.bernstein@gmail.com>
2018-12-04 12:44:27 -0500

    Test

---
+++ ASTInterpreter.hs
@@ -0,0 +1,55 @@
+module ASTInterpreter where
+
+
+import Prelude
+
+import qualified Data.Map as Map
+
+import Debug.Trace
+
+
+
+type Program = [Stmt]
+
+
+data Stmt = Def String [String] Stmt
+          | while Expr Stmt
+          | Block [Stmt]
+          | If Expr Stmt
+          | IfElse Expr Stmt Stmt
+          | Assign Expr Expr
+          | Id Expr 
+          | Ret Expr
+          | Print Expr
+          | Break
+          | Continue 
+          deriving Show
+
+
+
+data Expr = Val Int 
+          | Plus Expr Expr
+          | Sub Expr Expr 
+          | Mult Expr Expr 
+          | Divide Expr Expr
+          | Mods Expr Expr
+          | Eq Expr Expr
+          | NEq Expr Expr
+          | Lt Expr Expr
+          | LtEq Expr Expr
+          | Gt Expr Expr
+          | GtEq Expr Expr
+          | And Expr Expr 
+          | Or Expr Expr
+          | Not Expr Exp
+
+          | Var String
+
+
+          
+
+          deriving Show
+
+
+
+
