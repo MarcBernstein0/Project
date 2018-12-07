0611b88d1eeecb6acdf1be7205918d396f39962b
MarcBernstein0 <marcseth.bernstein@gmail.com>
2018-12-07 16:18:25 -0500

    Trying to use monads for eval

--- project/src/ASTInterpreter.hs
+++ project/src/ASTInterpreter.hs
@@ -5,36 +5,51 @@ import StatefulUnsafeMonad
 import Data.Map as Map
 
 
-data State = Map String Stmt -- TODO change to be the type of state, you have freedom for how you implement it
+type globalScope = Map String Stmt -- TODO change to be the type of state, you have freedom for how you implement it
+type localScope = Map String Integer
+
 
 test = [Def "main" [] (Block [Ret (Val 1)])]
 test2 = [Def "foo" ["x"] (Block [If (Or (Eq (Var "x") (Val 2)) (Eq (Var "x") (Val 3))) (Block [Ret (Var "y")])]),Def "main" [] (Block [Ret (Val 1)])]
 
-createGlobal :: [Stmt] -> (Map (String, [String]) Stmt)
+createGlobal :: [Stmt] -> globalScope
 createGlobal [] = Map.empty
 createGlobal ((Def "main" [] ast):rest) = Map.insert ("main",[]) ast Map.empty
 createGlobal ((Def name params ast):rest) = Map.insert (name, params) ast (createGlobal rest)
+-- Tried adding parameters to it
+
+-- createGlobal :: [Stmt] -> (Map String Stmt) 
+-- createGlobal [] = Map.empty
+-- createGlobal ((Def name params ast):rest) = Map.insert name ast (createGlobal rest)
+
+
+
 
 
 
 eval :: Program -> Maybe [String]
 eval (P program) = eval' (P program) (createGlobal program) Map.empty []
--- eval p = getPrints $ snd $ app (eval' p) undefined
---
--- getPrints :: State -> [String]
--- getPrints = undefined
---
--- eval' :: Program -> StatefulUnsafe State Int
--- eval' = undefined
+-- -- eval p = getPrints $ snd $ app (eval' p) undefined
+-- --
+-- -- getPrints :: State -> [String]
+-- -- getPrints = undefined
+-- --
+-- -- eval' :: Program -> StatefulUnsafe State Int
+-- -- eval' = undefined
 
---takes a program, buts it itno a 
-eval' :: Program -> (Map (String, [String]) Stmt) -> (Map String Integer) -> [String] -> Maybe [String]
-eval' p x y z = Nothing
+-- --takes a program, buts it itno a 
+-- eval' :: Program -> (Map (String, [String]) Stmt) -> (Map String Integer) -> [String] -> Maybe [String]
+-- eval' p globalScope localScope strList = case (Map.lookup ("main", []) globalScope) of
+--                                             Just ast -> evalStmt ast globalScope localScope strList
+--                                             Nothing -> Nothing
 
-evalStmt :: Stmt -> (Map String Stmt) -> (Map String Integer) -> [String] -> Maybe [String]
-evalStmt = undefined
+-- evalStmt :: Stmt -> (Map (String, [String]) Stmt) -> (Map String Integer) -> [String] -> Maybe [String]
+-- evalStmt (Assign varName expr) createGlobal localScope strList = let res = evalExpr expr globalScope localScope 
+--                                                                    in  
 
 
 
-evalExpr :: Expr -> (Map String Stmt) -> (Map String Integer) ->  Maybe Int
-evalExpr = undefined
+-- evalExpr :: Expr -> (Map (String, [String]) Stmt) -> (Map String Integer) ->  Maybe Int
+-- evalExpr (Val i) globalScope localScope = Just i
+-- evalExpr (Plus l r) globalScope localScope = let x = eval l globalScope localScope
+--                                                  y = eval r globalScope localScope
