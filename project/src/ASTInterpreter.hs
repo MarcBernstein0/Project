5c4c44dcd8896852bd996d75d2fbe7310a6012dc
MarcBernstein0 <marcseth.bernstein@gmail.com>
2018-12-08 14:27:54 -0500

    Trying some stuff out for the eval

--- project/src/ASTInterpreter.hs
+++ project/src/ASTInterpreter.hs
@@ -5,30 +5,35 @@ import StatefulUnsafeMonad
 import Data.Map as Map
 
 
-type globalScope = Map String Stmt -- TODO change to be the type of state, you have freedom for how you implement it
-type localScope = Map String Integer
+type GlobalScope = Map String Stmt -- TODO change to be the type of state, you have freedom for how you implement it
+type LocalScope = Map String Integer
 
 
 test = [Def "main" [] (Block [Ret (Val 1)])]
 test2 = [Def "foo" ["x"] (Block [If (Or (Eq (Var "x") (Val 2)) (Eq (Var "x") (Val 3))) (Block [Ret (Var "y")])]),Def "main" [] (Block [Ret (Val 1)])]
 
-createGlobal :: [Stmt] -> globalScope
+-- createGlobal :: [Stmt] -> globalScope
+-- createGlobal [] = G $ Map.empty
+-- createGlobal ((Def "main" [] ast):rest) = G $ Map.insert ("main",[]) ast Map.empty
+-- createGlobal ((Def name params ast):rest) = G $ Map.insert (name, params) ast (createGlobal rest)
+-- -- Tried adding parameters to it
+
+-- -- createGlobal :: [Stmt] -> (Map String Stmt) 
+-- -- createGlobal [] = Map.empty
+-- -- createGlobal ((Def name params ast):rest) = Map.insert name ast (createGlobal rest)
+
+createGlobal :: [Stmt] -> GlobalScope 
 createGlobal [] = Map.empty
-createGlobal ((Def "main" [] ast):rest) = Map.insert ("main",[]) ast Map.empty
-createGlobal ((Def name params ast):rest) = Map.insert (name, params) ast (createGlobal rest)
--- Tried adding parameters to it
-
--- createGlobal :: [Stmt] -> (Map String Stmt) 
--- createGlobal [] = Map.empty
--- createGlobal ((Def name params ast):rest) = Map.insert name ast (createGlobal rest)
-
-
-
-
+createGlobal ((Def funcName params ast): rest) = Map.insert funcName ast (createGlobal rest)  
 
 
 eval :: Program -> Maybe [String]
-eval (P program) = eval' (P program) (createGlobal program) Map.empty []
+eval (P program) = eval'
+
+--eval' :: Stmt -> 
+
+-- eval :: Program -> Maybe [String]
+-- eval (P program) = eval' (P program) (createGlobal program) Map.empty []
 -- -- eval p = getPrints $ snd $ app (eval' p) undefined
 -- --
 -- -- getPrints :: State -> [String]
