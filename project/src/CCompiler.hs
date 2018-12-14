31cea28e0226d8402ae66e9e107ba8866eaf9ed5
MarcBernstein0 <marcseth.bernstein@gmail.com>
2018-12-13 20:05:56 -0500

    compileExpr basic math works right now

--- project/src/CCompiler.hs
+++ project/src/CCompiler.hs
@@ -2,6 +2,12 @@ module CCompiler where
 
 import Ast
 import ICInterpreter
+import Data.Map as Map
+
+
+
+type Global = Map String (Int, [String])
+
 
 
 -- compile :: Program -> IC_Program
@@ -20,23 +26,63 @@ import ICInterpreter
 -- compileStmt Continue = undefined
 
 
--- backPatch 
+-- backPatch       TL     FL     CL     BL
+type BackPatch = ([Int], [Int], [Int], [Int])
+
+emptyBP :: BackPatch
+emptyBP = ([],[],[],[])
 
 
-temps = ["_t" ++ (show n) | n <- [0,1..]]
+type Temp = [Op]
 
-compileExpr :: Expr -> IC_Instruction ->  -> (IC_Instruction)
-compileExpr (Plus x y) = undefined
-compileExpr (Sub x y) = undefined
-compileExpr (Mult x y) = undefined
-compileExpr (Div x y) = undefined
-compileExpr (Mod x y) = undefined
-compileExpr (Eq x y) = undefined
-compileExpr (NEq x y) = undefined
-compileExpr (Lt x y) = undefined
-compileExpr (LtEq x y) = undefined
-compileExpr (Gt x y) = undefined
-compileExpr (GtEq x y) = undefined
-compileExpr (And x y) = undefined
-compileExpr (Or x y) = undefined
-compileExpr (Not x) = undefined
+temps :: Temp
+temps = [Var' ("_t" ++ (show n)) | n <- [0,1..]]
+
+
+run a = compileExpr a [] emptyBP temps Map.empty
+
+--going to need to have a global mapping for all the function localtions and parameters, Map String (Int, [String])
+
+--Compile :: Program -> IC_Program
+--Compile' :: Program -> IC_Program -> Temps -> Gmap
+
+
+-- compileStmt :: Stmt -> IC_Program -> Temps -> Gmap -> (IC_Program, BackPatch, Temp)
+-- compileStmt (Def funcName args body) ic temp g = 
+--     let (icNew, bp, tempNew) = compileStmt body ic temp (Map.insert funcName ((length ic), args) g)
+
+-- compileStmt [] ic temp g = (ic, emptyBP, temp)
+-- compileStmt (Assign var expr) ic temp g = 
+
+
+
+
+compileExpr :: Expr -> IC_Program -> BackPatch -> Temp -> Global -> (Op,IC_Program,BackPatch,Temp)
+compileExpr (Val i) ic bp (t:rest) g = (t, (ic ++ [Assign' (t) (Val' (fromIntegral i))]), bp, rest) -- going to need to change for testing purposes
+compileExpr (Plus l r) ic bp temp g = let (locl, icL, bpl, templ) = compileExpr l ic bp temp g
+                                          (locr, icR, bpr, (t:rest)) = compileExpr r icL bpl templ g
+									   in (t, (icR ++ [ (Plus' (t) (locl) (locr))]), bpr, rest)
+compileExpr (Sub x y) ic bp temp g = let  (locl, icL, bpl, templ) = compileExpr x ic bp temp g
+                                          (locr, icR, bpr, (t:rest)) = compileExpr y icL bpl templ g
+                                     in (t, (icR ++ [ (Minus' (t) (locl) (locr))]), bpr, rest)
+compileExpr (Mult x y) ic bp temp g = let (locl, icL, bpl, templ) = compileExpr x ic bp temp g
+                                          (locr, icR, bpr, (t:rest)) = compileExpr y icL bpl templ g
+                                      in (t, (icR ++ [ (Times' (t) (locl) (locr))]), bpr, rest)
+compileExpr (Div x y) ic bp temp g = let (locl, icL, bpl, templ) = compileExpr x ic bp temp g
+                                         (locr, icR, bpr, (t:rest)) = compileExpr y icL bpl templ g
+                                     in (t, (icR ++ [ (Div' (t) (locl) (locr))]), bpr, rest)
+compileExpr (Mod x y) ic bp temp g = let (locl, icL, bpl, templ) = compileExpr x ic bp temp g
+                                         (locr, icR, bpr, (t:rest)) = compileExpr y icL bpl templ g
+                                     in (t, (icR ++ [ (Mod' (t) (locl) (locr))]), bpr, [head rest])
+-- compileExpr (Eq x y) ic bp temp g = 
+--     let (locl, icL, bpl, templ) = compileExpr l ic bp temp g
+--         (locr, icR, bpr, (t:rest)) = compileExpr r icL bpl templ g
+--     in ()
+-- compileExpr (NEq x y) = undefined
+-- compileExpr (Lt x y) = undefined
+-- compileExpr (LtEq x y) = undefined
+-- compileExpr (Gt x y) = undefined
+-- compileExpr (GtEq x y) = undefined
+-- compileExpr (And x y) = undefined
+-- compileExpr (Or x y) = undefined
+-- compileExpr (Not x) = undefined
